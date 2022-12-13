54d3ff03c5829e3edf08c7b68fd2b20561f0d3d8
hanjonoka <joris.gallene@ensta-paris.fr>
2022-12-13 12:08:21 +0100

    VAZY JPP

--- src/main/java/com/ensta/myfilmlist/persistence/controller/FilmResource.java
+++ src/main/java/com/ensta/myfilmlist/persistence/controller/FilmResource.java
@@ -3,10 +3,11 @@ package com.ensta.myfilmlist.persistence.controller;
 import com.ensta.myfilmlist.dto.FilmDTO;
 import com.ensta.myfilmlist.exception.ControllerException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public interface FilmResource {
     ResponseEntity<List<FilmDTO>> getAllFilms() throws ControllerException;
-    ResponseEntity<FilmDTO> getFilmById() throws ControllerException;
+    ResponseEntity<FilmDTO> getFilmById(@PathVariable long id) throws ControllerException;
 }
--- src/main/java/com/ensta/myfilmlist/persistence/controller/impl/FilmResourceImpl.java
+++ src/main/java/com/ensta/myfilmlist/persistence/controller/impl/FilmResourceImpl.java
@@ -8,6 +8,7 @@ import com.ensta.myfilmlist.service.MyFilmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,9 +33,10 @@ public class FilmResourceImpl implements FilmResource {
         return ResponseEntity.status(500).build();
     }
 
+
     @Override
-    @GetMapping("/film/<ID_DU_FILM>")
-    public ResponseEntity<FilmDTO> getFilmById() throws ControllerException {
+    @GetMapping("/{id}")
+    public ResponseEntity<FilmDTO> getFilmById(@PathVariable long id) throws ControllerException {
 
         return null;
     }
